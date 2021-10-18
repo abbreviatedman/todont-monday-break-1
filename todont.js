c156ace72be411d82da0fca58aa10c3fc9f98631
Colin Jaffe <balloonasaurus@gmail.com>
2021-10-18 10:56:39 -0400

    Code up to break 1.

---
+++ todont.js
@@ -0,0 +1,8 @@
+const readlineSync = require('readline-sync')
+const getMenu = require('./getMenu')
+
+let response = readlineSync.question(getMenu())
+
+while (response !== 'q') {
+  response = readlineSync.question(getMenu())
+}
